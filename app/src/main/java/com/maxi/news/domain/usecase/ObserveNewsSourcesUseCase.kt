11a45a96d568b83a78fec8b74d2a28f5c53733a7
maxi <arora.anubhav93@gmail.com>
2026-03-04 16:54:26 +0530

    Add observe news sources use case

--- app/src/main/java/com/maxi/news/domain/usecase/ObserveNewsSourcesUseCase.kt
+++ app/src/main/java/com/maxi/news/domain/usecase/ObserveNewsSourcesUseCase.kt
@@ -1,4 +1,15 @@
 package com.maxi.news.domain.usecase
 
-class ObserveNewsSourcesUseCase {
+import com.maxi.news.common.Resource
+import com.maxi.news.domain.model.NewsSource
+import com.maxi.news.domain.repository.NewsSourcesRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveNewsSourcesUseCase @Inject constructor(
+    private val repository: NewsSourcesRepository
+) {
+
+    operator fun invoke(): Flow<Resource<List<NewsSource>>> =
+        repository.observeNewsSources()
 }
